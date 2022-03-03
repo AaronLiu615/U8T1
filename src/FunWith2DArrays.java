96fac66cf22ad2c967e99967a8ae6a97b22295cf
Elliot Zou <elliotz2@nycstudents.net>
2022-03-03 13:21:10 -0500

    added u8t2

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -31,4 +31,33 @@ public class FunWith2DArrays {
         System.out.print(array[row][columns]);
         System.out.println();
     }
+
+    public static double average(int[][] list){
+        int number = list.length * list[0].length;
+        double sum = 0;
+        for (int[] list1 : list){
+            for (int list2 : list1)
+            {
+                sum += list2;
+            }
+        }
+        return sum/number;
+    }
+
+    public static int[] indexFound(String[][] list, String target){
+        int[] index = new int[2];
+        for (int i = 0 ; i < list.length ; i++){
+            for (int x = 0; x < list[0].length; x++){
+                if (list[i][x].equals(target))
+                {
+                    index[0] = i;
+                    index[1] = x;
+                    return index;
+                }
+            }
+        }
+        index[0] = -1;
+        index[1] = -1;
+        return index;
+    }
 }
